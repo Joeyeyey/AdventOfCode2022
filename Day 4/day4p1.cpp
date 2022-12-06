1d6dcde5ad36636875a33283d2d4171f32656ac1
Jonathan Lee <thenameless0n3@gmail.com>
2022-12-06 15:32:13 -0800

    add day 4 files and input

---
+++ Day 4/day4p1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const string inputFileName = "input.txt";
+
+int main() {
+
+    ifstream inputFile(inputFileName);
+
+    string line;
+    while(getline(inputFile, line)) {
+
+    }
+
+    return 0;
+}
